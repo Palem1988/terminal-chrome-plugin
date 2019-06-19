37d6ddb6af26d9c339226ce0c95e1e78b049717b
Steven Enamakel <enamakel@cryptocontrol.io>
2019-06-19 21:13:53 +0300

    remove popup code

--- background.js
+++ background.js
@@ -9,6 +9,8 @@ var requestListener = function(details){
 		};
 	var i;
 
+	console.log('req', details)
+
 	for (i = 0; i < details.requestHeaders.length; ++i) {
 		if (details.requestHeaders[i].name.toLowerCase() === rule.name.toLowerCase()) {
 			flag = true;
@@ -24,8 +26,9 @@ var requestListener = function(details){
 		}
 	}
 
-	return {requestHeaders: details.requestHeaders};
+	return { requestHeaders: details.requestHeaders };
 };
+
 
 var responseListener = function(details){
 	var flag = false,
@@ -33,6 +36,8 @@ var responseListener = function(details){
 			"name": "Access-Control-Allow-Origin",
 			"value": "*"
 		};
+
+	console.log('res', details)
 
 	for (var i = 0; i < details.responseHeaders.length; ++i) {
 		if (details.responseHeaders[i].name.toLowerCase() === rule.name.toLowerCase()) {
@@ -44,9 +49,7 @@ var responseListener = function(details){
 	if(!flag) details.responseHeaders.push(rule);
 
 	if (accessControlRequestHeaders) {
-
 		details.responseHeaders.push({"name": "Access-Control-Allow-Headers", "value": accessControlRequestHeaders});
-
 	}
 
 	if(exposedHeaders) {
@@ -55,45 +58,56 @@ var responseListener = function(details){
 
 	details.responseHeaders.push({"name": "Access-Control-Allow-Methods", "value": "GET, PUT, POST, DELETE, HEAD, OPTIONS"});
 
-	return {responseHeaders: details.responseHeaders};
-
+	return { responseHeaders: details.responseHeaders };
 };
 
-/*On install*/
+/* On install */
 chrome.runtime.onInstalled.addListener(function(){
-	chrome.storage.local.set({'active': false});
-	chrome.storage.local.set({'urls': ["<all_urls>"]});
-	chrome.storage.local.set({'exposedHeaders': ''});
-	reload();
+	chrome.storage.local.set({ active: true });
+	chrome.storage.local.set({ urls: ["<all_urls>"] });
+	chrome.storage.local.set({ exposedHeaders: '' });
+
+	chrome.browserAction.onClicked.addListener(function(tab) {
+		toggle()
+	});
+
+	on();
 });
 
-/*Reload settings*/
-function reload() {
 
-	chrome.storage.local.get({'active': false, 'urls': ["<all_urls>"], 'exposedHeaders': ''}, function(result) {
-
-		exposedHeaders = result.exposedHeaders;
-
-		/*Remove Listeners*/
-		chrome.webRequest.onHeadersReceived.removeListener(responseListener);
-		chrome.webRequest.onBeforeSendHeaders.removeListener(requestListener);
-
-		if(result.active) {
-			chrome.browserAction.setIcon({path: "image.png"});
-
-			if(result.urls.length) {
-
-				/*Add Listeners*/
-				chrome.webRequest.onHeadersReceived.addListener(responseListener, {
-					urls: result.urls
-				},["blocking", "responseHeaders"]);
-
-				chrome.webRequest.onBeforeSendHeaders.addListener(requestListener, {
-					urls: result.urls
-				},["blocking", "requestHeaders"]);
-			}
-		} else {
-			chrome.browserAction.setIcon({path: "off.png"});
-		}
+function toggle () {
+	chrome.storage.local.get({ active: true }, function (result) {
+		if (result.active) off()
+		else on()
 	});
+}
+
+
+function on () {
+	chrome.storage.local.set({ active: true });
+
+	// Remove Listeners
+	chrome.webRequest.onHeadersReceived.removeListener(responseListener);
+	chrome.webRequest.onBeforeSendHeaders.removeListener(requestListener);
+
+	chrome.browserAction.setIcon({ path: "on.png" });
+
+	// Add Listeners
+	chrome.webRequest.onHeadersReceived
+		.addListener(responseListener, { urls: ["<all_urls>"] }, ["blocking", "responseHeaders"]);
+
+	chrome.webRequest.onBeforeSendHeaders
+		.addListener(requestListener,  { urls: ["<all_urls>"] },["blocking", "requestHeaders"]);
+}
+
+
+function off () {
+	chrome.storage.local.set({ active: false });
+
+	// remove listners
+	chrome.webRequest.onHeadersReceived.removeListener(responseListener);
+	chrome.webRequest.onBeforeSendHeaders.removeListener(requestListener);
+
+	// set icon
+	chrome.browserAction.setIcon({ path: "off.png" });
 }
