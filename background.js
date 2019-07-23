72d67ad6ea5ecbf6d9a1b50cc02742b63a595b83
Steven Enamakel <enamakel@cryptocontrol.io>
2019-07-23 12:41:55 +0530

    fixes for iframe and CORS

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
 	'https://terminal-test.cryptocontrol.io',
 	'https://terminal.cryptocontrol.io',
-	'http://localhost:3000'
+	'http://localhost:3000',
+	'https://cryptocontrol.io',
+	'chrome-extension://'
 ]
 
 
@@ -12,56 +14,65 @@ function _replaceOrInsert (array, key, value) {
 }
 
 
+function _remove (array, key) {
+	const index = array.findIndex(function (item) { return item.name.toLowerCase() === key.toLowerCase() })
+	if (index >= 0) array.splice(index, 1);
+}
+
+
 function responseHeadersListener (details) {
 	// iff the request is originating from the terminal
 	if (allowedOrigins.indexOf(details.initiator) === -1) return { };
+	// console.log(details.initiator, details.url)
 
 	// remove the iframe header
-	_replaceOrInsert(details.responseHeaders, 'x-frame-options', '*');
+	_remove(details.responseHeaders, 'x-frame-options');
+
+	// add cors headers
+	_replaceOrInsert(details.responseHeaders, 'access-control-allow-origin', details.initiator);
+	_replaceOrInsert(details.responseHeaders, 'access-control-request-method', 'GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH');
+	_replaceOrInsert(details.responseHeaders, 'access-control-request-headers', '*');
+	_replaceOrInsert(details.responseHeaders, 'X-Content-Type-Options', 'nosniff');
+
 	return { responseHeaders: details.responseHeaders };
 };
-
-/**
- * Switches the plugin on
- */
-function _on () {
-	console.log('plugin is switched on')
-
-	chrome.webRequest.onHeadersReceived.addListener(responseHeadersListener,
-		{ urls: ['<all_urls>'] }, ['blocking', 'responseHeaders', 'extraHeaders']
-	);
-
-	// set icon
-	chrome.browserAction.setIcon({ path: 'on.png' });
-}
 
 
 /* On install */
 chrome.runtime.onInstalled.addListener(function(){
-	_on(); // enable the plugin by default
-});
+	// Add response interceptor to modify requests
+	chrome.webRequest.onHeadersReceived.addListener(
+		responseHeadersListener,
+		{ urls: ['<all_urls>'] },
+		['blocking', 'responseHeaders', 'extraHeaders']
+	);
 
+	/**
+	 * Handle messages coming in from the terminal
+	 *
+	 * from https://stackoverflow.com/questions/55214828/how-to-stop-corb-from-blocking-requests-to-data-resources-that-respond-with-cors
+	 * and https://developer.chrome.com/apps/messaging
+	 * and https://www.chromium.org/Home/chromium-security/extension-content-script-fetches
+	 */
+	chrome.runtime.onMessageExternal.addListener(function (request, sender, sendResponse) {
+		if (request == 'version') return sendResponse(true);
 
-/**
- * Handle messages coming in from the terminal
- *
- * from https://stackoverflow.com/questions/55214828/how-to-stop-corb-from-blocking-requests-to-data-resources-that-respond-with-cors
- * and https://developer.chrome.com/apps/messaging
- */
-chrome.runtime.onMessageExternal.addListener(function (request, sender, sendResponse) {
-	if (request == 'version') return sendResponse(true)
+		const options = request.options || {}
+		// options.mode = 'no-cors'
 
-	fetch(request.url, request.options).then(function (response) {
-	  	return response.text().then(function (text) {
-			const result = {
-				body: text,
-				status: response.status,
-				statusText: response.statusText
-			};
+		fetch(request.url, options).then(function (response) {
+			return response.text().then(function (text) {
+				const result = {
+					body: text,
+					status: response.status,
+					statusText: response.statusText
+				};
 
-			sendResponse([result, null]);
-	  });
-	}, function (error) { sendResponse([null, error]); });
+				sendResponse([result, null]);
+		});
+		}, function (error) { sendResponse([null, error]); });
 
-	return true;
+		// Will respond asynchronously.
+		return true;
+	});
 });
